eaae9dfede71b8fb74ded4561560f8927f75aa89
leeyulgok <dbfrhr20@gmail.com>
2023-11-22 14:32:32 +0900

    feat: 계좌 읽고 쓰는 json 파일 및 핸들러 생성

---
+++ src/FileHandler.js
@@ -0,0 +1,34 @@
+import fs from "fs/promises";
+import { Console } from "@woowacourse/mission-utils";
+
+const FILE_PATH = "../json/accounts.json";
+
+export const writeJSONFile = async (account) => {
+  try {
+    const data = JSON.stringify(account, null, 2);
+    await fs.writeFile(FILE_PATH, data, "utf8");
+  } catch (error) {
+    Console.print(`${error.message}`);
+  }
+};
+
+export const readJSONFile = async () => {
+  try {
+    const data = await fs.readFile(FILE_PATH, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    Console.print(`${error.message}`);
+    return [];
+  }
+};
+
+export const findAccountByNumber = async (accountNumber) => {
+  try {
+    const data = await fs.readFile(FILE_PATH, "utf8");
+    const accounts = JSON.parse(data);
+    return accounts.find((account) => account.accountNumber === accountNumber);
+  } catch (error) {
+    Console.print(`${error.message}`);
+    return null;
+  }
+};
