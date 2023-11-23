2609da50542dba967b6901da91cccb1b8409a7f2
leeyulgok <dbfrhr20@gmail.com>
2023-11-23 16:46:33 +0900

    docs: 입력 예외처리 삭제
    예외는 무한루프로 인해 삭제

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -1,11 +1,6 @@
 import InputView from "../src/InputView.js";
 import { Console } from "@woowacourse/mission-utils";
 
-const ERROR_MESSAGE = {
-  INVALID_DEFAULT: "[ERROR] 유효하지 않은 입력입니다. 다시 한 번 입력해주세요.",
-  INVALID_ACCOUNT_NUMBER: "[ERROR] 유효하지 않은 계좌번호입니다. 다시 한 번 입력해주세요.",
-};
-
 jest.mock("@woowacourse/mission-utils", () => ({
   Console: {
     readLineAsync: jest.fn(),
@@ -25,11 +20,6 @@ describe("InputView 단위 테스트", () => {
     expect(uesrName).toBe("홍길동");
   });
 
-  test("readUserName: 예외적인 이름 입력", async () => {
-    Console.readLineAsync.mockResolvedValueOnce("hong");
-    await expect(InputView.readUserName()).rejects.toThrow(ERROR_MESSAGE.INVALID_DEFAULT);
-  });
-
   test("readExistsAccount: 정상적인 입력", async () => {
     Console.readLineAsync.mockResolvedValueOnce("1");
     const isTrue = await InputView.readExistsAccount()
@@ -40,17 +30,6 @@ describe("InputView 단위 테스트", () => {
     expect(isFalse).toBeFalsy();
   });
 
-  test("readExistsAccount: 예외적인 입력", async () => {
-    Console.readLineAsync.mockResolvedValueOnce("3");
-    await expect(InputView.readExistsAccount()).rejects.toThrow(ERROR_MESSAGE.INVALID_DEFAULT);
-
-    Console.readLineAsync.mockResolvedValueOnce("");
-    await expect(InputView.readExistsAccount()).rejects.toThrow(ERROR_MESSAGE.INVALID_DEFAULT);
-
-    Console.readLineAsync.mockResolvedValueOnce("b");
-    await expect(InputView.readExistsAccount()).rejects.toThrow(ERROR_MESSAGE.INVALID_DEFAULT);
-  });
-
   test("readForMakeAccountNumber: 정상적인 계좌번호 입력", async () => {
     Console.readLineAsync.mockResolvedValueOnce("123-4567");
     const accountNumber = await InputView.readForMakeAccountNumber();
@@ -58,21 +37,11 @@ describe("InputView 단위 테스트", () => {
     expect(accountNumber).toBe("123-4567");
   });
 
-  test("readForMakeAccountNumber: 예외적인 계좌번호 입력", async () => {
-    Console.readLineAsync.mockResolvedValueOnce("1234567");
-    await expect(InputView.readForMakeAccountNumber()).rejects.toThrow(ERROR_MESSAGE.INVALID_ACCOUNT_NUMBER);
-  });
-
   test("readUserPassword: 정상적인 비밀번호 입력", async () => {
     Console.readLineAsync.mockResolvedValueOnce("1234");
     const password = await InputView.readUserPassword();
 
     expect(password).toBe("1234");
-  });
-
-  test("readUserPassword: 예외적인 비밀번호 입력", async () => {
-    Console.readLineAsync.mockResolvedValueOnce("abcd");
-    await expect(InputView.readUserPassword()).rejects.toThrow(ERROR_MESSAGE.INVALID_DEFAULT);
   });
 
   test("readContinueTransaction: 정상적인 입력", async () => {
@@ -87,23 +56,10 @@ describe("InputView 단위 테스트", () => {
     expect(stopTransaction).toBeFalsy();
   });
 
-  test("readContinueTransaction: 예외적인 입력", async () => {
-    Console.readLineAsync.mockResolvedValueOnce("3");
-    await expect(InputView.readContinueTransaction()).rejects.toThrow(ERROR_MESSAGE.INVALID_DEFAULT);
-
-    Console.readLineAsync.mockResolvedValueOnce("");
-    await expect(InputView.readContinueTransaction()).rejects.toThrow(ERROR_MESSAGE.INVALID_DEFAULT);
-  });
-
   test("readUserAccountNumber: 정상적인 계좌번호 입력", async () => {
     Console.readLineAsync.mockResolvedValueOnce("987-6543");
     const accountNumber = await InputView.readUserAccountNumber();
 
     expect(accountNumber).toBe("987-6543");
-  });
-
-  test("readUserAccountNumber: 예외적인 계좌번호 입력", async () => {
-    Console.readLineAsync.mockResolvedValueOnce("9876543");
-    await expect(InputView.readUserAccountNumber()).rejects.toThrow(ERROR_MESSAGE.INVALID_ACCOUNT_NUMBER);
   });
 });
