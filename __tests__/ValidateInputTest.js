611f78a7f83175d4116586340f88678b9aaf01b6
leeyulgok <dbfrhr20@gmail.com>
2023-11-27 16:14:46 +0900

    feat: validateInput 테스트 코드 추가

--- __tests__/ValidateInputTest.js
+++ __tests__/ValidateInputTest.js
@@ -1,10 +1,24 @@
-import { validateYesOrNoInput } from "../src/validateInput";
+import { validateYesOrNoInput, validateMoney, validateWithdraw } from "../src/validateInput";
+import Account from "../src/Account.js";
 
 const ERROR_MESSAGE = {
   INVALID_DEFAULT: "[ERROR] 유효하지 않은 입력입니다. 다시 한 번 입력해주세요.",
+  INVALID_MONEY: "[ERROR] 유효하지 않은 금액입니다. 다시 한 번 입력해주세요.",
+  INVALID_WITHDRAW: "[ERROR] 출금하려는 금액이 더 많습니다.",
 };
 
 describe("validateInput 단위 테스트", () => {
+  const userName = "홍길동";
+  const accountNumber = "123-4567";
+  const password = "1234";
+  const balance = 1000;
+
+  let account;
+
+  beforeEach(() => {
+    account = new Account(userName, accountNumber, password, balance);
+  });
+
   test("validateYesOrNoInput: 정상적인 입력 처리", () => {
     expect(validateYesOrNoInput("1")).toBeTruthy();
     expect(validateYesOrNoInput("2")).toBeFalsy();
@@ -14,5 +28,29 @@ describe("validateInput 단위 테스트", () => {
     expect(() => validateYesOrNoInput("3")).toThrow(ERROR_MESSAGE.INVALID_DEFAULT);
     expect(() => validateYesOrNoInput("")).toThrow(ERROR_MESSAGE.INVALID_DEFAULT);
     expect(() => validateYesOrNoInput(0)).toThrow(ERROR_MESSAGE.INVALID_DEFAULT);
+  });
+
+  test("validateMoney: 정상적인 입력 처리", () => {
+    expect(validateMoney(10000)).toBe(10000);
+    expect(validateMoney(9000)).toBe(9000);
+    expect(validateMoney(100)).toBe(100);
+  });
+
+  test("validateMoney: 예외적인 입력 처리", () => {
+    expect(() => validateMoney(0)).toThrow(ERROR_MESSAGE.INVALID_MONEY);
+    expect(() => validateMoney(-1123)).toThrow(ERROR_MESSAGE.INVALID_MONEY);
+    expect(() => validateMoney()).toThrow(ERROR_MESSAGE.INVALID_MONEY);
+  });
+
+  test("validateWithdraw: 정상적인 입력 처리", () => {
+    expect(validateWithdraw(1000, account)).toBe(1000);
+    expect(validateWithdraw(1, account)).toBe(1);
+    expect(validateWithdraw(500, account)).toBe(500);
+  });
+
+  test("validateWithdraw: 예외적인 입력 처리", () => {
+    expect(() => validateWithdraw(10001, account)).toThrow(ERROR_MESSAGE.INVALID_WITHDRAW);
+    expect(() => validateWithdraw(100000, account)).toThrow(ERROR_MESSAGE.INVALID_WITHDRAW);
+    expect(() => validateWithdraw(100000000, account)).toThrow(ERROR_MESSAGE.INVALID_WITHDRAW);
   });
 });
--- src/validateInput.js
+++ src/validateInput.js
@@ -1,6 +1,7 @@
 const ERROR_MESSAGE = {
   INVALID_DEFAULT: "[ERROR] 유효하지 않은 입력입니다. 다시 한 번 입력해주세요.",
   INVALID_MONEY: "[ERROR] 유효하지 않은 금액입니다. 다시 한 번 입력해주세요.",
+  INVALID_WITHDRAW: "[ERROR] 출금하려는 금액이 더 많습니다.",
 };
 
 export const validateYesOrNoInput = (input) => {
@@ -25,7 +26,7 @@ export const validateMoney = (input) => {
 
 export const validateWithdraw = (money, account) => {
   if (account.balance < money) {
-    throw new Error("[ERROR] 출금하려는 금액이 더 많습니다.");
+    throw new Error(ERROR_MESSAGE.INVALID_WITHDRAW);
   }
 
   return money;
